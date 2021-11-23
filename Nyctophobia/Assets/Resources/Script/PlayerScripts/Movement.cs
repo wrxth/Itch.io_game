a53cc8faaf20e2729ae9aa5a47d857b7a76925ea
MilanDB <65161992+MilanDB@users.noreply.github.com>
2021-11-23 15:25:08 +0100

    im back baby
    git vond het leuk om een fork te maken van het project waar ik laat pas achter kwam.

---
+++ Nyctophobia/Assets/Resources/Script/PlayerScripts/Movement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    public float currentMoveSpeed;
+    private float minMoveSpeed;
+    [SerializeField] private float staminaCount;
+    private float maxStamina;
+    private bool rechargeStage = false;
+
+    [SerializeField] private float staminaDrain;
+
+    public bool canWalk;
+
+    public int playerHealth;
+
+    private Slider staminaBar;
+
+    private void Start()
+    {
+        staminaBar = GameObject.Find("Stamina Bar").GetComponent<Slider>();
+        
+
+        maxStamina = staminaCount;
+        minMoveSpeed = currentMoveSpeed;
+    }
+    void Update()
+    {
+        if (playerHealth <= 0)
+        {
+            canWalk = false;
+        }
+
+        staminaCount = Mathf.Clamp(staminaCount, 0, maxStamina);
+
+        if (staminaCount > 0 && Input.GetKey(KeyCode.LeftShift) && !rechargeStage)
+        {
+            staminaCount -= staminaDrain * Time.deltaTime;
+            StaminaUI();
+            currentMoveSpeed = minMoveSpeed * 1.75f;
+        }
+
+        else if (staminaCount < maxStamina)
+        {
+            if(staminaCount <= 0)
+            {
+                rechargeStage = true;
+            }
+            staminaCount += staminaDrain * Time.deltaTime;
+            StaminaUI();
+            currentMoveSpeed = minMoveSpeed;
+        }
+        if(staminaCount == maxStamina)
+        {
+            rechargeStage = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (canWalk)
+        {
+            float h = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+            float v = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+
+            Vector3 movement = new Vector3(h, 0.0f, v);
+
+            transform.Translate(movement * currentMoveSpeed, Space.World);
+
+            if (movement != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(movement);
+            }
+        }
+    }
+
+    public void StaminaUI()
+    {
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = staminaCount;
+    }
+}
---
+++ Nyctophobia/Assets/Resources/Script/PlayerScripts/PlayerCam.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    private Transform playerPos;
+    private Vector3 cameraOffset;
+
+    [SerializeField]private int CameraZoomMin, CameraCurrentZoomIndex, CameraZoomMax;
+    private void Start()
+    {
+        cameraOffset = transform.position;
+    }
+    void Update()
+    {
+        playerPos = GameObject.Find("Player").transform;
+        transform.position = playerPos.transform.position + cameraOffset;
+
+        if(CameraCurrentZoomIndex > CameraZoomMin && Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            Camera.main.fieldOfView--;
+            CameraCurrentZoomIndex--;
+        }
+        else if (CameraCurrentZoomIndex < CameraZoomMax && Input.GetAxis("Mouse ScrollWheel") < 0f) // backward
+        {
+            Camera.main.fieldOfView++;
+            CameraCurrentZoomIndex++;
+        }
+    }
+}
