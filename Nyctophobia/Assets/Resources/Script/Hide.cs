e8850891981351f95591762a1a8c6989ca6ce07a
MilanDB <65161992+MilanDB@users.noreply.github.com>
2021-11-24 15:44:26 +0100

    v1 update

--- Nyctophobia/Assets/Resources/Script/Hide.cs
+++ Nyctophobia/Assets/Resources/Script/Hide.cs
@@ -5,26 +5,28 @@ using UnityEngine;
 public class Hide : MonoBehaviour, Iinteract
 {
     public bool insideCloset;
-    public List<GameObject> exitPositions;
+   
     private Movement mov;
     private GameObject exitPosition;
-    private GameObject player;
+    private GameObject Player;
 
     private void Start()
     {
-        exitPositions = new List<GameObject>();
         exitPosition = gameObject.transform.GetChild(1).gameObject;
-        player = GameObject.FindGameObjectWithTag("Player");
+        Player = GameObject.FindGameObjectWithTag("Player");
         mov = player.GetComponent<Movement>();
+        
     }
 
     public void Interact()
     {
         if (!insideCloset) // If the player is not inside the closet it will transport them inside the closet and set the bool to true
         {
+            
             GameObject.FindGameObjectWithTag("Player").transform.position = gameObject.transform.position;
             insideCloset = true;
             mov.canWalk = false;
+            
         }
 
         else if (insideCloset) // If the player in inside the closet it will transport them outside the closet and set the bool to false
